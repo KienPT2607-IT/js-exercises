d339e9a5189a45d40496e68fb2b083bbf88c323c
Lê Nam Giang <146799513+mor-giangln@users.noreply.github.com>
2024-06-10 22:28:37 +0700

    Update ex6.js

--- ex6.js
+++ ex6.js
@@ -1,6 +1,8 @@
 /* Bài 6. Viết 1 function có tác dụng biến 1 chuỗi thành chỉ viết hoa từ đầu tiên.
 Tham số truyền vào là 1 chuỗi.
 Kết quả là 1 chuỗi mới chỉ viết hoa từ đầu tiên. Ví dụ "morSofware" sẽ được convert thành "Morsofware". */
+
+// Lấy ký tự đầu tiên (index 0) trong chuỗi ghép với chuỗi sau khi đã cắt ký tự đầu tiên
 function CapitalizeAWord(word) {
    let newWord = word[0].toUpperCase()
    for (let i = 1; i < word.length; i++) {
