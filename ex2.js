ed15e4a4224936ae33d27b654b45d24c317f57e5
Lê Nam Giang <146799513+mor-giangln@users.noreply.github.com>
2024-06-10 22:11:04 +0700

    Update ex2.js

--- ex2.js
+++ ex2.js
@@ -2,6 +2,8 @@
 Cho 1 chuỗi, hãy viết hàm có tác dụng sao chép chuỗi đó lên 10 lần, ngăn cách nhau bởi ký tự "-".
 Ví dụ cho chuỗi "123" thì kết quả sẽ là "123-123-123-123-123-123-123-123-123-123". 
 */
+
+// Dùng các method có sẵn của JS. Tạo mảng sau đó join các elements thay vì loop rồi ghép chuỗi
 function muliplyString(arg) {
    let result = ""
    for (let index = 0; index < 10; index++) {
