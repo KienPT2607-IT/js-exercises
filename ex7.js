582499598b6c2e37e73648a71a0fcd546cad016a
Lê Nam Giang <146799513+mor-giangln@users.noreply.github.com>
2024-06-10 22:32:30 +0700

    Update ex7.js

--- ex7.js
+++ ex7.js
@@ -1,5 +1,5 @@
 /* Bài 7. Cho 1 mảng các số bất kỳ. Tạo ra 1 mảng mới chỉ chứa các số lẻ lấy ra từ mảng trên và sắp xếp theo thứ tự giảm dần. */
-
+// Dùng các method của JS. Không for lồng
 let arr = [45, 83, 12, 57, 29, 61, 78, 3, 90, 42, 24, 67, 85, 30, 11, 4, 56, 93, 18, 7, 3, 3]
 let newArr = []
 
