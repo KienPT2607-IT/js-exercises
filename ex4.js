cd61bb85c394179d7a5424e614f3c437c49d14a1
Lê Nam Giang <146799513+mor-giangln@users.noreply.github.com>
2024-06-10 22:24:10 +0700

    Update ex4.js

--- ex4.js
+++ ex4.js
@@ -15,9 +15,9 @@ function calculateFactorial(num) {
   result = calculateFactorrialRescursive(num)
   return result;
 }
-
+// Đệ quy nhìn thì ngắn hơn nhưng sẽ dẫn tới stack limit nếu tham số lớn
 function calculateFactorrialRescursive(num) {
-  if (num == 1) return 1;
+  if (num == 1) return 1; // Có thể dùng ||
   if (num == 0) return 1;
   return num * calculateFactorrialRescursive(num - 1);
 }
