0f035d7101a87514b86014634ab606db5686f001
KienPT <phamkien.8a@gmail.com>
2024-06-11 09:29:40 +0700

    fix the C to F fomula, add doc

--- ex1.js
+++ ex1.js
@@ -4,12 +4,17 @@ Viết một hàm chuyển đổi nhiệt độ từ thang Celsius chuyển sang
 vd output là: "60°C bằng 140 °F"
 */
 // Sai công thức
-function convertCelsiusToFahrenhei(celsius) {
-   return celsius + 33.8
+/**
+ * 
+ * @param {int} celsius 
+ * @returns The Fahrenheit degree transformed from Celsius
+ */
+function convertCelsiusToFahrenheit(celsius) {
+   return (celsius * 9 / 5) + 32
 }
-let a = convertCelsiusToFahrenhei(19)
+let a = convertCelsiusToFahrenheit(19)
 console.log(a);
 
-let convertCToF = (celsius) => celsius + 33.8
+let convertCToF = (celsius) => (celsius * 9 / 5) + 32
 a = convertCToF(100)
 console.log(a);
