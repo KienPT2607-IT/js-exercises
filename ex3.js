ab461b808fa1ceddcd37e02289fbc0f79bc06a7e
KienPT <phamkien.8a@gmail.com>
2024-06-11 09:38:55 +0700

    add doc to ex3.js

--- ex3.js
+++ ex3.js
@@ -1,6 +1,12 @@
 /* Bài 3:
 Sử dụng câu lệnh if để viết 1 hàm với 2 tham số bất kỳ, kiểm tra xem 2 tham số có phải là number không và tìm ra số lớn nhất trong 2 số đó. */
 
+/**
+ * 
+ * @param {int} num1 
+ * @param {int} num2 
+ * @returns 
+ */
 function compareNumbers(num1, num2) {
    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
       return null;
@@ -8,5 +14,5 @@ function compareNumbers(num1, num2) {
    return (num1 > num2) ? num1 : num2
 }
 
-let result = decideLargerNumber("gg", 6)
+let result = compareNumbers("gg", 6)
 console.log(result);
